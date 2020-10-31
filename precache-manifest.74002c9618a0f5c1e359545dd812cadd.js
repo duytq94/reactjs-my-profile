9193d2b2b5a6be4a6b132d513f26fcf4f36b624f
duytq94 <tranquangduy7994@gmail.com>
2020-10-31 10:28:24 +0700

    Updates

---
+++ precache-manifest.74002c9618a0f5c1e359545dd812cadd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6494ffca818af1e3317e80c1841a4e",
+    "url": "/reactjs-my-profile/index.html"
+  },
+  {
+    "revision": "8f7cdc0f9d4cc3dfb4bb",
+    "url": "/reactjs-my-profile/static/css/main.c805a85d.chunk.css"
+  },
+  {
+    "revision": "510c2a60357032561a89",
+    "url": "/reactjs-my-profile/static/js/2.297fef43.chunk.js"
+  },
+  {
+    "revision": "8f7cdc0f9d4cc3dfb4bb",
+    "url": "/reactjs-my-profile/static/js/main.bb1f029b.chunk.js"
+  },
+  {
+    "revision": "575dae9eb2fde37972ab",
+    "url": "/reactjs-my-profile/static/js/runtime~main.81122daf.js"
+  },
+  {
+    "revision": "bfcc9865d491cd0dbe64176f55eb9c9e",
+    "url": "/reactjs-my-profile/static/media/avatar.bfcc9865.jpg"
+  },
+  {
+    "revision": "d2c74e7b430633770db40a3b975ddecc",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-Bold.d2c74e7b.otf"
+  },
+  {
+    "revision": "5cf37d909218a592aa661568c9727270",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-DemiBold.5cf37d90.otf"
+  },
+  {
+    "revision": "6faa576bd8cf054aaa8a7a250c1933ff",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-Light.6faa576b.otf"
+  },
+  {
+    "revision": "b15969e2dab8d106cbf185ef081ba9f5",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-LightItalic.b15969e2.otf"
+  },
+  {
+    "revision": "028be64712f87065750bcc96cd6ab1b4",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-Medium.028be647.otf"
+  },
+  {
+    "revision": "5ea7b2a9b6b7dd2a4f701cf9220c986b",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-Regular.5ea7b2a9.otf"
+  }
+]);
